4ce9b59d7cc28061ed2c0e9aecb1e478e4fd63f5
oaldsp <otaviopepe@alunos.utfpr.edu.br>
2026-05-02 20:47:46 -0300

    implementação inicial do notification

---
+++ micro_services/notification.py
@@ -0,0 +1,38 @@
+import json
+from encryption import generate_keys, generate_signature
+
+class Notification:
+    rabbitmq = None
+    private_key = None
+    public_key = None
+
+    def __init__(self, rabbitmq):
+        self.private_key, self.public_key = generate_keys()
+        self.rabbitmq = rabbitmq
+        rabbitmq.register_service_public_key("notification", self.public_key)
+
+    def callback(self, event_json):
+        event = json.loads(event_json) # Converte o JSON para dicionário
+        content = event["content"]
+
+        category = content.get("category", "geral")
+        routing_key = f"promocao.{category}"
+
+        signature = generate_signature(content)
+        self.rabbitmq.publish("notification", signature, routing_key, content)
+
+        print(f"Notificação enviada: {routing_key}")
+
+if __name__ == "__main__":
+    notification = Notification()
+    channel = notification.rabbitmq.channel
+
+    channel.queue_declare(queue='notification_queue')
+
+    channel.queue_bind(exchange='promotions', queue='notification_queue', routing_key='promotion.published')
+    channel.queue_bind(exchange='promotions', queue='notification_queue', routing_key='promocao.hot_deal')
+
+    channel.basic_consume(queue='notification_queue', on_message_callback=notification.callback, auto_ack=True)
+
+    print("Notificacao rodando...")
+    channel.start_consuming()
